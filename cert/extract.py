06b7ba30843f6a1dbefb1b4830fb28cdbe3ae5ab
Валерий В. Исаков <isakovvv@yoomoney.ru>
2023-01-11 22:00:19 +0300

    function to extract host and port from url, update ROADMAP

--- cert/extract.py
+++ cert/extract.py
@@ -3,20 +3,26 @@ import ssl
 from urllib.parse import urlparse
 
 class Extract:
-    #@staticmethod
-    #def extract_port(url: str):
-        #port = 443
-        #o = urlparse(url)
-        #o.hostname
-        #o.port
+    @staticmethod
+    def extract_host_port(url: str):
+        port = 443
+        parsed_url = urlparse(url)
+        host = parsed_url.hostname
+        if parsed_url.port:
+            port = parsed_url.port
+
+        return host, port
+
 
     @staticmethod
     def pem_to_x509(cert_data: str):
         return OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, str.encode(cert_data))
 
+
     def fetch_ssl(self, url: str):
+        host, port = self.extract_host_port(url)
         result = {}
-        connection = ssl.create_connection((url, 443))
+        connection = ssl.create_connection((host, port))
         context = ssl.SSLContext()
         sock = context.wrap_socket(connection, server_hostname=url)
         server_certificate = self.pem_to_x509(ssl.DER_cert_to_PEM_cert(sock.getpeercert(True)))
