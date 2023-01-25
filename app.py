6b2cb63dd268f273eceef09118208cae4fda70df
Валерий В. Исаков <isakovvv@yoomoney.ru>
2023-01-25 21:29:16 +0300

    update cert output, add buttons for save and cancel

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from forms import UrlForm
+from flask import Flask, render_template, flash
+from forms import UrlForm, SaveCertForm
 from cert.extract import Extract
 
 app = Flask(__name__)
@@ -10,10 +10,25 @@ app.config['SECRET_KEY'] = 'a very hard to guess secret super key'
 def index():
     url = None
     ssl = None
-    form = UrlForm()
-    if form.validate_on_submit():
-        url = form.url.data
+    visibility = 'hidden'
+    check_url_form = UrlForm()
+    save_url_form = SaveCertForm()
+
+    if check_url_form.validate_on_submit():
+        url = check_url_form.url.data
         ssl = Extract()
         ssl = ssl.fetch_ssl(url)
-        form.url.data = ''
-    return render_template('index.html', form=form, url=url, ssl=ssl)
+        check_url_form.url.data = ''
+
+        if ssl:
+            visibility = 'visible'
+
+    if save_url_form.validate_on_submit():
+        if save_url_form.save.data:
+            print(save_url_form.subject.data)
+            flash('success')
+        if save_url_form.cancel.data:
+            visibility = 'hidden'
+            flash('cancel')
+
+    return render_template('index.html', check_url_form=check_url_form, save_url_form=save_url_form,  url=url, ssl=ssl, visibility=visibility)
--- forms.py
+++ forms.py
@@ -1,13 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField, validators)
+                     RadioField, validators, SubmitField)
 from wtforms.validators import InputRequired, Length, ValidationError
 from urllib.parse import urlparse
 
 class UrlForm(FlaskForm):
     url = StringField(label = 'Enter website url', render_kw={'placeholder': 'website.com'}, validators=[InputRequired()])
+    check = SubmitField()
 
     def validate_url(form, url):
             u = urlparse(url.data)
             if not all([u.scheme, u.netloc]):
                 raise ValidationError('Url is not valid, please enter https://example.com')
+
+class SaveCertForm(FlaskForm):
+    subject = StringField(label = 'subject')
+    notbefore = StringField(label = 'notBefore')
+    notafter = StringField(label = 'notAfter')
+    issuer = StringField(label = 'issuer')
+    subjectaltname = StringField(label = 'subjectAltName')
+    save = SubmitField()
+    cancel = SubmitField()
