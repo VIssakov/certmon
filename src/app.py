4392f9d2d7264b364197e7b4eb8e4dde070232ff
Валерий В. Исаков <user.email=isakovvv@yoomoney.ru>
2023-10-29 19:21:53 +0300

    move models to external file

--- src/app.py
+++ src/app.py
@@ -11,34 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Config(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    parameter = db.Column(db.String(100), unique=True, nullable=False)
-    data = db.Column(db.String(500), nullable=False)
-
-    def __repr__(self):
-        return f'<Config {self.parameter}>'
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    chat_id = db.Column(db.String(500), nullable=True)
-
-    def __repr__(self):
-        return f'<User {self.name}>'
-
-class Certs(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    subject = db.Column(db.String(500), nullable=False)
-    not_before = db.Column(db.String(100), nullable=False)
-    not_after = db.Column(db.String(100), nullable=False)
-    issuer = db.Column(db.String(500), nullable=False)
-    extension_count = db.Column(db.String(1000), nullable=True)
-    subject_altName = db.Column(db.String(1000), nullable=False)
-
-    def __repr__(self):
-        return f'<Certificate {self.subject}>'
+from models import Config, Users, Certs
 
 @app.before_request
 def create_tables():
